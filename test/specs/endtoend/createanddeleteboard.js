59fe7f2e1e7a9a682d869f8bc443e54fb33ca7ca
mugilanmonish <147095455+mugilanmonish@users.noreply.github.com>
2025-01-15 14:40:09 +0530

    Update createanddeleteboard.js

--- test/specs/endtoend/createanddeleteboard.js
+++ test/specs/endtoend/createanddeleteboard.js
@@ -3,8 +3,7 @@ import TrelloLoginPage from "../../pageobjects/trellologin.page.js";
 import TrelloBoardsPage from "../../pageobjects/trelloboards.page.js";
 import TrelloJavaScriptBoardPage from "../../pageobjects/trellojavascriptboard.page.js";
 import TrelloLogoutPage from "../../pageobjects/trellologout.page.js";
-
-// This E2E script for Create and Delete a board in Trello. 
+ 
 describe('Trello', async () => {
     it('Create And Delete Board', async () => {
         await browser.url("/")
